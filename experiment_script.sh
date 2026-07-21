8091bbb8c91e4406c6360b5de717b4ce0e0d9292
maiemile <maiemileh@gmail.com>
2026-07-21 13:04:21 +0300

    update the sh script template

--- experiment_script.sh
+++ experiment_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --account=XXXXXX   # Choose the project to be billed
-#SBATCH --time=06:00:00             # Maximum duration of the job. Upper limit depends on partition.
+#SBATCH --time=00:30:00             # Maximum duration of the job. Upper limit depends on partition.
+#SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
 #SBATCH --cpus-per-task=20           # How many processors work on one task. Upper limit depends on number of CPUs per node.
 #SBATCH --mem-per-cpu=2G            # Minimum memory required per usable allocated CPU.  Default units are megabytes.
 #SBATCH --partition=small            # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 
-srun /projappl/XXXXXX/DESDEO/DESDEO/.venv/bin/python run_experiments.py
+srun /projappl/XXXXXX/DESDEO/DESDEO/.venv/bin/python main.py
