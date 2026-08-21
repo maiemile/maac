c1a0325686a32a3f87ce911c2a84c2f22bf4315b
maiemile <maiemileh@gmail.com>
2026-08-21 12:18:23 +0300

    use DESDEO for AHD

--- calc_indicator_values.py
+++ calc_indicator_values.py
@@ -1,7 +1,6 @@
 # Code by @maiemile
 
 from desdeo.tools.indicators_unary import igd_plus_indicator, distance_indicators
-from moocore import avg_hausdorff_dist
 from pathlib import Path
 import numpy as np
 import pandas as pd
@@ -60,7 +59,7 @@ def calc_ind_val_problem(run_id:int, problem_id:int, indicators:list[str], ind_v
         if indicator == "igd_plus":
             ind_val = igd_plus_indicator(normalized_archive, normalized_pf_approx).igd_plus
         if indicator == "ahd":
-            ind_val = avg_hausdorff_dist(normalized_archive, normalized_pf_approx)
+            ind_val = distance_indicators(normalized_archive, normalized_pf_approx).ahd
         ind_res[indicator] = ind_val
 
     # create the update statement
